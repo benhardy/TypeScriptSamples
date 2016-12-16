7bd0b152f2c40b3816613b3ce755c6ee14da0fc5
Ben Hardy <benhardy+github@gmail.com>
2016-12-16 08:28:54 -0500

    move raytracer camera back slightly

--- raytracer/raytracer.ts
+++ raytracer/raytracer.ts
@@ -261,7 +261,7 @@ function defaultScene(): Scene {
                  { pos: new Vector(1.5, 2.5, 1.5), color: new Color(0.07, 0.07, 0.49) },
                  { pos: new Vector(1.5, 2.5, -1.5), color: new Color(0.07, 0.49, 0.071) },
                  { pos: new Vector(0.0, 3.5, 0.0), color: new Color(0.21, 0.21, 0.35) }],
-        camera: new Camera(new Vector(3.0, 2.0, 4.0), new Vector(-1.0, 0.5, 0.0))
+        camera: new Camera(new Vector(5.0, 2.0, 4.0), new Vector(-1.0, 1.0, 0.0))
     };
 }
 
